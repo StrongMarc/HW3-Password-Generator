00abe222d9eb5adffdd2b982afb54aba198cceb1
Marc Strong <marcstrong@yahoo.com>
2020-02-22 01:41:57 -0600

    password critria check for incorrect input

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var criteria = prompt(`Select password criteria to include:  Press "1" for lenth or Press "2" for character types`);
-console.log(criteria)
+
+function passwprdLength () {
+  var length = prompt(`Choose a legnth of at least 8 characters and no more than 128 characters`)
+  console.log(length)
+}
+
+function criteriaType () {
+  var criteria = prompt(`Select password criteria to include:  Press "1" for lenth or Press "2" for character types`);
+  console.log(criteria)
+  if (isNaN(criteria)) {
+    criteriaType ()
+  }
+  else if (criteria < 1 || criteria > 2) {
+    criteriaType ()
+  }
+}
+criteriaType();
 
 // Write password to the #password input
 function writePassword() {
