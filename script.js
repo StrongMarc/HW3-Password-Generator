bf61571adfb987ca10477432986603413f9e14be
Marc Strong <marcstrong@yahoo.com>
2020-02-23 01:28:36 -0600

    working password generator, working flow to alternate criteria type

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var typeCount = 0;
 //code for user input to define password length
 function passwordLength () {
-  var length = prompt(`Choose a legnth of at least 8 characters and no more than 128 characters (Enter: 8-128)`)
+  var length = prompt(`Choose a length of at least 8 characters and no more than 128 characters (Enter: 8-128)`)
   console.log(length)
   if (length == null) {
     alert("User cancelled the prompt.  Returning to previous prompt");
@@ -15,8 +16,17 @@ function passwordLength () {
   else if (length < 8 || length > 128) {
     passwordLength ();
   }
+  else {
+    typeCount++
+    console.log(typeCount)
+    if (typeCount==1){
+      alert("Thank you. Now select character type criteria")
+      characterTypes ();
+    }
+  }
+  // return {parseInt(length);
 }
-
+// console.log(y)
 //code for user input to define types of characters to be 
 // utilized for the generate password
 function characterTypes () {
@@ -28,6 +38,18 @@ function characterTypes () {
   console.log(numeric)
   var specialChar = confirm(`Password criteria to include special characters?`)
   console.log(specialChar)
+  if (lowercase == true || uppercase == true || numeric == true || specialChar == true) {
+    typeCount++
+    console.log(typeCount)
+    if (typeCount==1){
+      alert("Thank you. Now select password length criteria")
+      passwordLength ();
+    }
+  }
+  else {
+    alert(`You must select "OK" for at least one type. Try again.`)
+    characterTypes ();
+  }
 }
 
 //code for user input to select which criteria type to 
@@ -48,7 +70,6 @@ function criteriaType () {
     passwordLength();
   }
   else if (criteria == 2){
-    console.log(criteria)
     characterTypes();
   }
 }
@@ -61,15 +82,29 @@ generateBtn.onclick = function() {
 // document.getElementById('generate').onclick = function() {
 // 	criteriaType();
 // }
+function generatePassword(length) {
+  var password           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    password += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(8);
   var passwordText = document.querySelector("#password");
-
+  // console.log(generatePassword(length));
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// document.getElementsByTagName("h1")[0].addEventListener("click", change);
+// function change(event){
+//     document.getElementsByTagName("h1")[0].innerHTML = "YOU CLICKED ME!"
+//     console.log(event)
